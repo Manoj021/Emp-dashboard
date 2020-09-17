97faca6c050b2094a4d2dd24ad02de63bf871363
Manoj021 <mukthavaram.manoj@gmail.com>
2020-09-18 01:16:51 +0530

    Updates

---
+++ precache-manifest.a1074be71d6133bba32b1e3c2882cc87.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6908ad05b750564db039",
+    "url": "/Emp-dashboard/static/css/main.50bd0012.chunk.css"
+  },
+  {
+    "revision": "6908ad05b750564db039",
+    "url": "/Emp-dashboard/static/js/main.a74cfa18.chunk.js"
+  },
+  {
+    "revision": "47d9c2ea3126652933b6",
+    "url": "/Emp-dashboard/static/js/runtime~main.27fc0c2d.js"
+  },
+  {
+    "revision": "995aa8ad2d61cb7dfc72",
+    "url": "/Emp-dashboard/static/js/2.939906f1.chunk.js"
+  },
+  {
+    "revision": "fc047347b17f7df7ff288d78c8c281cf",
+    "url": "/Emp-dashboard/static/media/avatar.fc047347.png"
+  },
+  {
+    "revision": "2b8fd602de5889375a92a2b78d45437e",
+    "url": "/Emp-dashboard/static/media/WCheckmark.2b8fd602.svg"
+  },
+  {
+    "revision": "28b7a594665e16fafe61441864579039",
+    "url": "/Emp-dashboard/static/media/clock.28b7a594.svg"
+  },
+  {
+    "revision": "6aaf5764065b3c9a9f941ddf35dd0e5c",
+    "url": "/Emp-dashboard/static/media/Assigned.6aaf5764.svg"
+  },
+  {
+    "revision": "da5e1febd2b0219ae3461ab58dd515e3",
+    "url": "/Emp-dashboard/static/media/Pending.da5e1feb.svg"
+  },
+  {
+    "revision": "aaa950a0dc5f5bd8ac9725480fb5b884",
+    "url": "/Emp-dashboard/index.html"
+  }
+];
